1b1cb6b59cc8b9f1aa8b318a35634478e6c4668e
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-01-30 17:32:35 +0600

    fineal problem update

--- array.js
+++ array.js
@@ -92,3 +92,9 @@ console.log(Array.isArray(info)); // true
 // 1. Create two arrays of your choice.
 // 2. Use the concat method to combine the two arrays into a new array.
 // 3. Print both the original arrays and the combined array using console.log().
+
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+const result = array1.concat(array2)
+console.log(result);
